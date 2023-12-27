5248d2b396de5f5722d2b84b92d6e1f6483be410
gdapriana <gedeapriana36@gmail.com>
2023-12-28 02:16:54 +0800

    🃏mobile init

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+const App = () => {
+  return  (
+    <View style={{width: "100%", height: "100%", backgroundColor: "purple", flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Hai</Text>
+    </View>
+  )
+}
+
+export default App
